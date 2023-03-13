6b390b401a3dbca9a92fad70767ee6d5a696b4e5
wlarkspur <newacon@naver.com>
2023-03-13 21:05:40 +0200

    set

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,10 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className="font-semibold">$27</span>
         </div>
-        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500 block">
+        <button
+          className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500 block
+          "
+        >
           Checkout
         </button>
       </div>
@@ -57,9 +60,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500"></button>
-              <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
-              <button className="w-5 h-5 rounded-full bg-teal-500"></button>
+              <button className="w-5 h-5 rounded-full bg-yellow-500 transition focus:ring-2 ring-offset-2 ring-yellow-500  "></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500 transition focus:ring-2 ring-offset-2 ring-indigo-500"></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500 transition focus:ring-2 ring-offset-2 ring-teal-500"></button>
             </div>
             <div className="flex items-center space-x-5">
               <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8  text-xl text-gray-500">
