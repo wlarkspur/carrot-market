fe37d1003fad0dcd17cebb5be7d1329323a88d0b
wlarkspur <newacon@naver.com>
2023-03-14 22:16:38 +0200

    tutorial tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,25 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
-      <div className="bg-white flex flex-col justify-between p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
+    <div className="bg-slate-400  xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <div className="bg-white dark:bg-black flex flex-col justify-between p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold dark:text-white text-3xl">
+          Select Item
+        </span>
         <div className="flex justify-between my-2">
-          <span className="text-gray-500 ">Grey Chair</span>
-          <span className="font-semibold">$19</span>
+          <span className="text-gray-500 dark:text-gray-100 ">Grey Chair</span>
+          <span className="font-semibold dark:text-white ">$19</span>
         </div>
         <div className="flex justify-between my-2">
-          <span className="text-gray-500 ">Grey Chair</span>
-          <span className="font-semibold">$19</span>
+          <span className="text-gray-500 dark:text-gray-100 ">Grey Chair</span>
+          <span className="font-semibold dark:text-white">$19</span>
         </div>
         <div className=" flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$27</span>
         </div>
         <button
-          className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500 block
+          className="mt-5 bg-blue-500 dark:bg-black dark:border-white dark:border text-white p-3 text-center rounded-xl w-3/4 mx-auto dark:hover:bg-white hover:bg-teal-500 hover:text-black dark:hover:text-black active:bg-yellow-500 focus:bg-red-500 block
           "
         >
           Checkout
