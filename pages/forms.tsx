bbbf065c57b841fe4679757c7edda1fcf9497f07
wlarkspur <newacon@naver.com>
2023-03-27 21:44:39 +0200

    React-hook-form Recap

---
+++ pages/forms.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FieldErrors } from "react-hook-form/dist/types";
+
+// Better validation
+// Better Errors (set, clear, display)
+// Have control over inputs
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export default function Forms() {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
+    console.log("im valid baby");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", { required: "Email is required" })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: "Password is required" })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
