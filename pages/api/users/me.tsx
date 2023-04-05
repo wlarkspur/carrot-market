2c6d4414b59cd4cf98e3ba0494ea415c07320a51
wlarkspur <newacon@naver.com>
2023-04-05 23:42:23 +0200

    Token handler updated

---
+++ pages/api/users/me.tsx
@@ -0,0 +1,17 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@/libs/server/withHandlers";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
+  });
+  res.status(200).end();
+}
+
+export default withApiSession(withHandler("GET", handler));
