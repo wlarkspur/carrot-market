a9b24ec6c90732885be81d8638bd8a44938d8c64
wlarkspur <newacon@naver.com>
2023-04-13 20:59:41 +0200

    product page backend+front

---
+++ carrot-market/pages/api/products/[id].ts
@@ -0,0 +1,35 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@/libs/server/withHandlers";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { id } = req.query;
+  const product = await client.product.findUnique({
+    where: {
+      id: +id!.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  console.log(product);
+  res.json({ ok: true, product });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
