fbaf488b09473de71841123197447de62c3318a1
wlarkspur <newacon@naver.com>
2023-04-18 21:50:54 +0200

    ADD community form & post api

---
+++ carrot-market/pages/api/posts/[id].ts
@@ -0,0 +1,59 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@/libs/server/withHandlers";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+  const post = await client.post.findUnique({
+    where: {
+      id: +id!,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wondering: true,
+        },
+      },
+    },
+  });
+  if (!post) res.status(404).json({ ok: false, error: "Not found post" });
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
--- carrot-market/pages/community/write.tsx
+++ carrot-market/pages/community/write.tsx
@@ -1,17 +1,44 @@
 import Button from "@/components/button";
 import Layout from "@/components/layout";
+import TextArea from "@/components/textarea";
+import useMutation from "@/libs/client/useMutation";
+import { Post } from "@prisma/client";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [post, { loading, data }] = useMutation<WriteResponse>("/api/posts");
+  const onValid = (data: WriteForm) => {
+    if (loading) return; //User가 Loading중 여러번 클릭하여 data posting방지
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout canGoBack title="Write Post">
-      <form className="p-4 space-y-4">
-        <textarea
-          className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
+      <form onSubmit={handleSubmit(onValid)} className="p-4 space-y-4">
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
           rows={4}
           placeholder="Ask a question!"
         />
-        <Button text="Submit" />
+        <Button text={loading ? "loading..." : "Submit"} />
       </form>
     </Layout>
   );
