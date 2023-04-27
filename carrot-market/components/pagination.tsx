5ff0dfc378842d0ac8e5e4634be19fd2d9070766
wlarkspur <newacon@naver.com>
2023-04-28 00:14:44 +0200

    pagination complete

--- carrot-market/components/pagination.tsx
+++ carrot-market/components/pagination.tsx
@@ -1,8 +1,5 @@
 import { cls } from "@/libs/client/utils";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { set } from "react-hook-form";
 
 interface pageProps {
   currentPage: number;
@@ -17,36 +14,41 @@ const Pagination = ({
   totalPage,
   pageSize,
 }: pageProps) => {
-  const [isDisabled, setIsDisabled] = useState(true);
   const pages = totalPage
     ? Array.from({ length: totalPage }, (_, i) => i + 1)
     : [];
-  const router = useRouter();
-  const pageNow = router.query.page;
   const startIndex = Math.max(0, currentPage - 3);
   const visiblePages = pages.slice(startIndex, startIndex + 5);
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === totalPage;
 
-  console.log(router.query.page);
   /*  const startPage = Math.max(0, currentPage - 2);
   const endPage = Math.min(totalPage || 1, currentPage + 2);
   const slicedPages = pages.slice(startPage - 1, endPage); */
   return (
-    <div className="bg-gray-200 flex w-[50%] justify-center rounded-md">
-      {/* <Link
-        onClick={() => handlePageChange(+pageNow! - 1)}
-        className="flex items-center"
-        href={`/streams?page=${+pageNow! - 1}`}
-        aria-disabled={+pageNow! === 0 ? isDisabled : false}
-      >
-        {"prev"}
-      </Link> */}
+    <div className="bg-gray-200 flex w-[55%] justify-center rounded-md">
+      {isFirstPage ? (
+        <span className="text-gray-400 flex items-center">prev</span>
+      ) : (
+        <Link legacyBehavior href={`/streams?page=${currentPage - 1}`}>
+          <a
+            onClick={() => handlePageChange(currentPage - 1)}
+            className="flex items-center "
+          >
+            prev
+          </a>
+        </Link>
+      )}
       {visiblePages.map((page) => (
-        <div key={page} className="rounded-sm py-2 px-3 ">
+        <div
+          key={page}
+          className="rounded-md flex items-center  hover:bg-sky-200 "
+        >
           <Link
             onClick={() => handlePageChange(page)}
             href={`/streams?page=${page}`}
             className={cls(
-              "font-semibold px-1",
+              "font-semibold mx-1 px-3 py-2 border-blue-500",
               currentPage === page ? "text-red-500" : "cursor-pointer"
             )}
           >
@@ -54,14 +56,18 @@ const Pagination = ({
           </Link>
         </div>
       ))}
-      {/* <Link
-        onClick={() => handlePageChange(+pageNow! + 1)}
-        className="flex items-center"
-        href={`/streams?page=${+pageNow! + 1}`}
-        aria-disabled={+pageNow! === totalPage}
-      >
-        {"next"}
-      </Link> */}
+      {isLastPage ? (
+        <span className="text-gray-400 flex items-center"> next</span>
+      ) : (
+        <Link legacyBehavior href={`/streams?page=${currentPage + 1}`}>
+          <a
+            onClick={() => handlePageChange(currentPage + 1)}
+            className="flex items-center "
+          >
+            next
+          </a>
+        </Link>
+      )}
     </div>
   );
 };
