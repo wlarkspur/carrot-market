6981e9f4ef70887d4bad745f6559bbd73d0fb575
wlarkspur <newacon@naver.com>
2023-05-06 15:44:53 +0200

    Securing strea

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/router";
 import { useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import useSWR from "swr";
+import streams from "../api/streams";
 
 interface StreamMessage {
   message: string;
@@ -79,39 +80,65 @@ const StreamMessage: NextPage = () => {
     <Layout canGoBack>
       <div className="py-10 px-4 space-y-4">
         <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video  " />
-        <h1 className=" text-gray-800 font-semibold text-3xl mt-2">
-          {data?.stream?.name}
-        </h1>
-        <span className="text-2xl mt-2 text-gray-900">
-          ${data?.stream?.price}
-        </span>
-        <p className="my-6 text-gray-700">{data?.stream?.description}</p>
-        <div className="py-10 pb-16 h-[50vh] overflow-y-scroll space-y-4">
-          {data?.stream.messages.map((message) => (
-            <Message
-              key={message.id}
-              message={message.message}
-              reversed={message.user.id === user?.id}
-            />
-          ))}
-          <div ref={scrollRef} />
+        <div className="mt-5">
+          <h1 className=" text-gray-800 font-semibold text-3xl mt-2">
+            {data?.stream?.name}
+          </h1>
+          <span className="text-2xl mt-2 text-gray-900">
+            ${data?.stream?.price}
+          </span>
+          <p className="my-6 text-gray-700">{data?.stream?.description}</p>
+          <div className="bg-orange-200 p-2 rounded-md flex flex-col space-y-2 ">
+            <span className="font-bold text-gray-700">
+              Stream Keys<span className="text-red-500">(secret)</span>
+            </span>
+            <span className="font-semibold text-md text-gray-700 flex items-center  ">
+              <div className="flex felx-col">
+                <div>
+                  <span className="flex items-center">URL:</span>
+                  <span className="flex items-center">Key:</span>
+                </div>
+                <div className="flex flex-col items-start mt-1 ml-2 text-gray-800 font-bold text-[13px]">
+                  <span className="flex items-center ml">
+                    {data?.stream.cloudflareUrl}
+                  </span>
+                  <span className="flex items-center mt-1.5">
+                    {data?.stream.cloudflareKey}
+                  </span>
+                </div>
+              </div>
+            </span>
+          </div>
         </div>
-        <div className="fixed py-2 w-full mx-auto max-w-md bottom-2 inset-x-0">
-          <form
-            onSubmit={handleSubmit(onValid)}
-            className="flex relative items-center"
-          >
-            <input
-              type="text"
-              {...register("message", { required: true })}
-              className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
-            />
-            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-              <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 cursor-pointer text-sm font-bold text-white">
-                &rarr;
-              </button>
-            </div>
-          </form>
+        <div>
+          <p className="font-bold text-3xl mb-2">Live Chat</p>
+          <div className="py-10 pb-16 h-[50vh] overflow-y-scroll space-y-4 border-[3px] border-orange-400 rounded-md p-1">
+            {data?.stream.messages.map((message) => (
+              <Message
+                key={message.id}
+                message={message.message}
+                reversed={message.user.id === user?.id}
+              />
+            ))}
+            <div ref={scrollRef} />
+          </div>
+          <div className="fixed py-2 w-full mx-auto max-w-md bottom-2 inset-x-0">
+            <form
+              onSubmit={handleSubmit(onValid)}
+              className="flex relative items-center"
+            >
+              <input
+                type="text"
+                {...register("message", { required: true })}
+                className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+              />
+              <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+                <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 cursor-pointer text-sm font-bold text-white">
+                  &rarr;
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </Layout>
