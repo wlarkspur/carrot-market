5530ed2cd2a1d3592ec2a908161b17eaea6028dc
wlarkspur <newacon@naver.com>
2023-06-01 00:02:41 +0200

    RSC Tutorial done

--- pages/coins.server.tsx
+++ pages/coins.server.tsx
@@ -3,24 +3,48 @@ import { Suspense } from "react";
 const cache: any = {};
 function fetchData(url: string) {
   if (!cache[url]) {
-    throw fetch(url)
-      .then((r) => r.json())
-      .then((json) => (cache[url] = json.slice(0, 50)));
+    throw Promise.all([
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => (cache[url] = json)),
+      new Promise((resolve) =>
+        setTimeout(resolve, Math.round(Math.random() * 5000))
+      ),
+    ]);
   }
   return cache[url];
 }
 
+function Coin({ id, name, symbol }: any) {
+  const {
+    quotes: {
+      USD: { price },
+    },
+  } = fetchData(`https://api.coinpaprika.com/v1/tickers/${id}`);
+  console.log(price);
+  return (
+    <span>
+      {name} / {symbol}: ${price}
+    </span>
+  );
+}
+
 function List() {
   const coins = fetchData("https://api.coinpaprika.com/v1/coins");
-  console.log(coins);
+  const coinList = coins.slice(0, 10);
   return (
-    <ul>
-      {coins.map((coin: any) => (
-        <li key={coin.id}>
-          {coin.name} / {coin.symbol}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h4>List is done</h4>
+      <ul>
+        {coinList.map((coin: any) => (
+          <li key={coin.id}>
+            <Suspense fallback={`Coin ${coin.name} is Loading...`}>
+              <Coin {...coin} />
+            </Suspense>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
